1cca74113b93d8755a266fe5669e87a9207f8d9d
Dalufishe <fishawe87@gmail.com>
2023-08-28 18:45:07 +0800

    feat: about component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,7 @@ import useVolumeViewer from './hooks/useVolumeViewer';
 import Info from './components/Info';
 import Hint from './components/Hint';
 import Social from './components/Social';
+import About from './components/About';
 
 export default function App() {
 
@@ -20,6 +21,7 @@ export default function App() {
         <div className="relative">
             <Info />
             <Hint />
+            <About />
             <Social />
             <canvas className='webgl'></canvas>
         </div>
--- client/src/components/Info.jsx
+++ client/src/components/Info.jsx
@@ -4,8 +4,9 @@ import { cn } from '../utils/cn'
 export default function Info() {
     return (
         <div className="fixed mx-3 my-1">
-            <h1 className='text-2xl font-bold'>Volume Viewer</h1>
-            <h3 className={cn("text-gray-200")}>A web-based volumetric renderer for Vesuvius Challenge</h3>
+            <h1 className='text-2xl font-bold'>VC Whiteboard</h1>
+            <h3 className={cn("text-gray-200")}>Make sense of complex topics in Vesuvius Challenge</h3>
         </div>
     )
 }
+
