44b447db4d8a24c98cc0036201265294cdee2e5b
Dalufishe <fishawe87@gmail.com>
2023-12-10 18:13:51 +0800

    feat: iframe snapshot

--- src/components/UrlCards/UrlCard/UrlCard.jsx
+++ src/components/UrlCards/UrlCard/UrlCard.jsx
@@ -2,21 +2,19 @@ import { useEffect, useRef, useState } from "react"
 import { cn } from "../../../utils/cn"
 import { css } from "@emotion/css"
 import { Slider } from "../../ui/Slider"
-import { useHover } from "../../../hooks/useHover"
+import html2canvas from 'html2canvas';
 
 export default function UrlCard({ card }) {
 
     const inputRef = useRef(null)
+    useEffect(() => {
+        inputRef.current?.focus()
+    }, [])
 
     const [inupt, setInput] = useState("");
     const [url, setUrl] = useState("");
 
-    const [hover, isHover] = useHover();
     const [isVisable, setIsVisable] = useState(true);
-
-    const handleClose = () => {
-        PubSub.publish("onUrlCardDelete", { id: card.id })
-    }
 
     const handleEnter = () => {
         setIsVisable(true)
@@ -28,13 +26,16 @@ export default function UrlCard({ card }) {
         }
     }
 
+    const handleClose = () => {
+        PubSub.publish("onUrlCardDelete", { id: card.id })
+    }
+
+
     const handleFlip = () => {
         setFlip(!flip)
     }
 
-    useEffect(() => {
-        inputRef.current?.focus()
-    }, [])
+
 
     const [flip, setFlip] = useState(false);
     useEffect(() => {
@@ -51,59 +52,98 @@ export default function UrlCard({ card }) {
         PubSub.publish("onCardScaleChange", { id: card.id, scale })
     }, [scale, card.id])
 
+    // const handleFileOnClick = async (file: File) => {
+    //     const arraybuffer = await file.arrayBuffer();
+    //     const blob = new Blob([arraybuffer], { type: file.name });
+    //     const text = await file.text();
+    //     PubSub.publish("onImgCardGenerate", {
+    //         id: nanoid(),
+    //         fileType: file.type,
+    //         fileName: file.name,
+    //         blob,
+    //         text,
+    //     });
+    // };
+
+    const iframeRef = useRef();
+
+    const takeSnapshot = () => {
+        if (!iframeRef.current) return;
+
+        html2canvas(iframeRef.current.contentWindow.document.body)
+            .then(canvas => {
+                const image = canvas.toDataURL('image/png');
+                fetch(image)
+                    .then(res => res.blob())
+                    .then(blob => {
+                        // 在这里处理 blob
+                        console.log(blob);
+                    });
+            })
+    };
+
     return <div
-        ref={hover}
         onMouseEnter={handleEnter}
         onMouseLeave={handleLeave}
         className={cn(
-            "fixed translate-x-[-50%] translate-y-[-50%]", "flex flex-col p-2", css(`
+            "fixed translate-x-[-50%] translate-y-[-50%]", css(`
             top: ${card.positionScreen.y}px;
             left: ${card.positionScreen.x}px;
             width: ${card.widthScreen}px;
             height:${card.heightScreen}px;
    
         `))}>
-        <div style={{ opacity: isVisable ? 1 : 0 }}
-            className="flex flex-col gap-0.5 transition-opacity duration-700 items-end">
-            <div className="flex gap-2 text-black">
-                <p className="text-white">flip</p>
-                <div className="py-1 w-32 bg-transparent flex items-center">
-                    <label>
-                        <input
-                          type="checkbox"
-                          name="flip"
-                          checked={flip}
-                          onChange={handleFlip}
-                        />
-                    </label>
+        <div className="flex gap-2 items-end justify-end">
+            <div className="flex flex-col p-2 gap-1">
+                <div style={{ opacity: isVisable ? 1 : 0 }}
+                    className="flex flex-col transition-opacity duration-700 items-end">
+                    <div className="flex gap-2 text-black">
+                        <p className="text-white">flip</p>
+                        <div className="w-32 bg-transparent flex items-center">
+                            <input
+                                type="checkbox"
+                                name="flip"
+                                checked={flip}
+                                onChange={handleFlip}
+                            />
+                        </div>
+                    </div>
+                </div>
+                <div style={{ opacity: isVisable ? 1 : 0 }}
+                    className="flex flex-col gap-0.5 transition-opacity duration-700 items-end">
+                    <div className="flex gap-2 text-black">
+                        <p className="text-white">scale</p>
+                        <div className="py-1 w-32 bg-transparent flex items-center">
+                            <Slider
+                                value={[scale]}
+                                onValueChange={(v) => {
+                                    setScale(v[0])
+                                }}
+                                className="w-full" max={2} step={0.01} />
+                        </div>
+                    </div>
+                </div>
+                <div style={{ opacity: isVisable ? 1 : 0 }}
+                    className="flex flex-col gap-0.5 transition-opacity duration-700 items-end">
+                    <div className="flex gap-2 text-black">
+                        <p className="text-white">rotation</p>
+                        <div className="py-1 w-32 bg-transparent flex items-center">
+                            <Slider
+                                value={[rotation]}
+                                onValueChange={(v) => {
+                                    setRotation(v[0])
+                                }}
+                                className="w-full" max={360} step={1} />
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div style={{ opacity: isVisable ? 1 : 0 }}
-            className="flex flex-col gap-0.5 transition-opacity duration-700 items-end">
-            <div className="flex gap-2 text-black">
-                <p className="text-white">scale</p>
-                <div className="py-1 w-32 bg-transparent flex items-center">
-                    <Slider
-                        value={[scale]}
-                        onValueChange={(v) => {
-                            setScale(v[0])
-                        }}
-                        className="w-full" max={2} step={0.01} />
-                </div>
-            </div>
-        </div>
-        <div style={{ opacity: isVisable ? 1 : 0 }}
-            className="flex flex-col gap-0.5 transition-opacity duration-700 items-end">
-            <div className="flex gap-2 text-black">
-                <p className="text-white">rotation</p>
-                <div className="py-1 w-32 bg-transparent flex items-center">
-                    <Slider
-                        value={[rotation]}
-                        onValueChange={(v) => {
-                            setRotation(v[0])
-                        }}
-                        className="w-full" max={360} step={1} />
+            <div className="flex flex-col p-2 gap-1">
+                <div style={{ opacity: isVisable ? 1 : 0 }}
+                    className="flex flex-col transition-opacity duration-700 items-end">
+                    <div className="flex gap-2 text-black">
+                        <button className="bg-[#DDD] rounded-lg p-1 hover:bg-[#FFF]" onClick={takeSnapshot}>spanshot</button>
+                    </div>
                 </div>
             </div>
         </div>
@@ -128,6 +168,7 @@ export default function UrlCard({ card }) {
                 type="text" />}
         </div>
         <iframe
+            ref={iframeRef}
             style={{
                 width: card.widthScreen - 16,
                 height: card.heightScreen,
